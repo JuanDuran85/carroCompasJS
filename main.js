9a466246ef5a49ce5e846e67a1acb9ac110355d4
JuanDuran85 <duran85juan@gmail.com>
2019-11-27 18:04:14 -0300

    aumentando, disminuyendo y elimiando productos del carro

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ botonesDelDomACarro.forEach(botonDomCarro => {
                     <img class="cart__item__image" src="${producto.imagen}" alt="${producto.nombre}">
                     <h3 class="cart__item__name">${producto.nombre}</h3>
                     <h3 class="cart__item__price">${producto.precio}</h3>
-                    <button class="btn btn--primary btn--small" data-action="DECRESE_ITEM">&minus;</button>
+                    <button class="btn btn--primary btn--small btn--danger" data-action="DECRESE_ITEM">&minus;</button>
                     <h3 class="cart__item__quantity">${producto.cantidad}</h3>
                     <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
                     <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
@@ -39,14 +39,46 @@ botonesDelDomACarro.forEach(botonDomCarro => {
             //cargamos el arreglo del carro vacio con los productos seleccionados por boton
             carro.push(producto);
             botonDomCarro.innerText = "In Cart"; //cambiando nombre en boton
+            botonDomCarro.disabled = true; //desactivando boton
 
             //leyendo botones presionados en el carro y actuando segun el boton activado
             const carroItemsDOM = carroDOM.querySelectorAll('.cart__item');
             carroItemsDOM.forEach(carroItemDom => {
+                
                 if (carroItemDom.querySelector('.cart__item__name').innerText === producto.nombre) {
+
+                    //incrementado cantidadcon boton +
                     carroItemDom.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', ()=>{
                         producto.cantidad++;
                         carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
+                        carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').classList.remove('btn--danger');
+                    });
+
+                    //disminuyendo cantidadcon boton -
+                    carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').addEventListener('click', ()=>{
+                        if (producto.cantidad > 1) {
+                            producto.cantidad--;
+                            carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
+                        } else {
+                            carroItemDom.classList.add('cart__item--removed');
+                            setTimeout(()=>carroItemDom.remove(),290);
+                            carro = carro.filter(carroItem => (carroItem.nombre !== producto.nombre));
+                            botonDomCarro.innerText = "Add to Cart"; //cambiando nombre en boton
+                            botonDomCarro.disabled = false; //reactivando boton
+                            
+                        };
+                        if (producto.cantidad === 1) {
+                            carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').classList.add('btn--danger');
+                        }
+                    });
+
+                    //Eliminando producto del carro con boton X
+                    carroItemDom.querySelector('[data-action="REMOVE_ITEM"]').addEventListener('click', ()=>{
+                        carroItemDom.classList.add('cart__item--removed');
+                        setTimeout(()=>carroItemDom.remove(),290);
+                        carro = carro.filter(carroItem => (carroItem.nombre !== producto.nombre));
+                        botonDomCarro.innerText = "Add to Cart"; //cambiando nombre en boton
+                        botonDomCarro.disabled = false; //reactivando boton
                     });
                 };
             });
