a74687ae036b65316e3b4f5505936322a6c18315
JuanDuran85 <duran85juan@gmail.com>
2019-11-27 16:29:45 -0300

    agregando botones y funciones al carro con productos

--- main.js
+++ main.js
@@ -16,7 +16,8 @@ botonesDelDomACarro.forEach(botonDomCarro => {
         const producto = { //se guarda cada elemento del boton accionado
             imagen: productoDelDOM.querySelector('.product__image').getAttribute('src'),
             nombre: productoDelDOM.querySelector('.product__name').innerText,
-            precio: productoDelDOM.querySelector('.product__price').innerText
+            precio: productoDelDOM.querySelector('.product__price').innerText,
+            cantidad: 1
         };
         
         //se revisa que productos estan el carro, si es asi, no se agrega
@@ -27,13 +28,28 @@ botonesDelDomACarro.forEach(botonDomCarro => {
             carroDOM.insertAdjacentHTML('beforeend',`
                 <div class="cart__item">
                     <img class="cart__item__image" src="${producto.imagen}" alt="${producto.nombre}">
-                    <h3 class="class__item__name">${producto.nombre}</h3>
-                    <h3 class="class__item__price">${producto.precio}</h3>
+                    <h3 class="cart__item__name">${producto.nombre}</h3>
+                    <h3 class="cart__item__price">${producto.precio}</h3>
+                    <button class="btn btn--primary btn--small" data-action="DECRESE_ITEM">&minus;</button>
+                    <h3 class="cart__item__quantity">${producto.cantidad}</h3>
+                    <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+                    <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
                 </div>
             `);
             //cargamos el arreglo del carro vacio con los productos seleccionados por boton
             carro.push(producto);
             botonDomCarro.innerText = "In Cart"; //cambiando nombre en boton
+
+            //leyendo botones presionados en el carro y actuando segun el boton activado
+            const carroItemsDOM = carroDOM.querySelectorAll('.cart__item');
+            carroItemsDOM.forEach(carroItemDom => {
+                if (carroItemDom.querySelector('.cart__item__name').innerText === producto.nombre) {
+                    carroItemDom.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', ()=>{
+                        producto.cantidad++;
+                        carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
+                    });
+                };
+            });
         };
     });
 });
