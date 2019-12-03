f980fe34752215e3f80827747f11bafb944dd6af
JuanDuran85 <duran85juan@gmail.com>
2019-12-03 16:57:57 -0300

    restructurando el codigo con funciones

--- main.js
+++ main.js
@@ -15,68 +15,15 @@ const botonesDelDomACarro = document.querySelectorAll('[data-action="ADD_TO_CART
 
 if (carro.length > 0) {
   carro.forEach(producto => {
-     //se agrega productos al carro al presionar el boton
-     carroDOM.insertAdjacentHTML('beforeend',`
-        <div class="cart__item">
-            <img class="cart__item__image" src="${producto.imagen}" alt="${producto.nombre}">
-            <h3 class="cart__item__name">${producto.nombre}</h3>
-            <h3 class="cart__item__price">${producto.precio}</h3>
-            <button class="btn btn--primary btn--small ${(producto.cantidad === 1 ? 'btn--danger':'')}" data-action="DECRESE_ITEM">&minus;</button>
-            <h3 class="cart__item__quantity">${producto.cantidad}</h3>
-            <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
-            <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
-        </div>
-    `);
+     //se agrega productos al carro al presionar el boton (llamado a funcion)
+     insertandoItemDom(producto)
 
     //se bloquean los botones de los productos que estan el carro y vienen del local storage
     botonesDelDomACarro.forEach(botonDomCarro => {
         const productoDelDOM = botonDomCarro.parentNode;
         if (productoDelDOM.querySelector('.product__name').innerText === producto.nombre) {
-            botonDomCarro.innerText = "In Cart"; //cambiando nombre en boton
-            botonDomCarro.disabled = true; //desactivando boton    
-            const carroItemsDOM = carroDOM.querySelectorAll('.cart__item');
-            carroItemsDOM.forEach(carroItemDom => {
-                
-                if (carroItemDom.querySelector('.cart__item__name').innerText === producto.nombre) {
-
-                    //incrementado cantidadcon boton +
-                    carroItemDom.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', ()=>{
-                        producto.cantidad++;
-                        carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
-                        carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').classList.remove('btn--danger');
-                        localStorage.setItem('carroLS',JSON.stringify(carro));
-                    });
-
-                    //disminuyendo cantidadcon boton -
-                    carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').addEventListener('click', ()=>{
-                        if (producto.cantidad > 1) {
-                            producto.cantidad--;
-                            carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
-                            localStorage.setItem('carroLS',JSON.stringify(carro));
-                        } else {
-                            carroItemDom.classList.add('cart__item--removed');
-                            setTimeout(()=>carroItemDom.remove(),290);
-                            carro = carro.filter(carroItem => (carroItem.nombre !== producto.nombre));
-                            localStorage.setItem('carroLS',JSON.stringify(carro));
-                            botonDomCarro.innerText = "Add to Cart"; //cambiando nombre en boton
-                            botonDomCarro.disabled = false; //reactivando boton                           
-                        };
-                        if (producto.cantidad === 1) {
-                            carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').classList.add('btn--danger');
-                        }
-                    });
-
-                    //Eliminando producto del carro con boton X
-                    carroItemDom.querySelector('[data-action="REMOVE_ITEM"]').addEventListener('click', ()=>{
-                        carroItemDom.classList.add('cart__item--removed');
-                        setTimeout(()=>carroItemDom.remove(),290);
-                        carro = carro.filter(carroItem => (carroItem.nombre !== producto.nombre));
-                        localStorage.setItem('carroLS',JSON.stringify(carro));
-                        botonDomCarro.innerText = "Add to Cart"; //cambiando nombre en boton
-                        botonDomCarro.disabled = false; //reactivando boton
-                    });
-                };
-            });
+            //llamando a funcion de manejo de botones
+            manejoDeBotones(botonDomCarro,producto);
         };
     });
   });  
@@ -97,74 +44,86 @@ botonesDelDomACarro.forEach(botonDomCarro => {
         const estaEnCarro = (carro.filter(carroItem =>(carroItem.nombre === producto.nombre)).length > 0);
         
         if (estaEnCarro === false) {
-            //se agrega productos al carro al presionar el boton
-            carroDOM.insertAdjacentHTML('beforeend',`
-                <div class="cart__item">
-                    <img class="cart__item__image" src="${producto.imagen}" alt="${producto.nombre}">
-                    <h3 class="cart__item__name">${producto.nombre}</h3>
-                    <h3 class="cart__item__price">${producto.precio}</h3>
-                    <button class="btn btn--primary btn--small ${(producto.cantidad === 1 ? 'btn--danger':'')}"" data-action="DECRESE_ITEM">&minus;</button>
-                    <h3 class="cart__item__quantity">${producto.cantidad}</h3>
-                    <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
-                    <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
-                </div>
-            `);
+            //se agrega productos al carro al presionar el boton (llamado a funcion)
+            insertandoItemDom(producto)
+
             //cargamos el arreglo del carro vacio con los productos seleccionados por boton
             carro.push(producto);
 
             //guardando en localstorege los resultados del carro
             localStorage.setItem('carroLS',JSON.stringify(carro));
-
-            botonDomCarro.innerText = "In Cart"; //cambiando nombre en boton
-            botonDomCarro.disabled = true; //desactivando boton
-
-            //leyendo botones presionados en el carro y actuando segun el boton activado
-            const carroItemsDOM = carroDOM.querySelectorAll('.cart__item');
-            carroItemsDOM.forEach(carroItemDom => {
-                
-                if (carroItemDom.querySelector('.cart__item__name').innerText === producto.nombre) {
-
-                    //incrementado cantidadcon boton +
-                    carroItemDom.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', ()=>{
-                        producto.cantidad++;
-                        carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
-                        carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').classList.remove('btn--danger');
-                        localStorage.setItem('carroLS',JSON.stringify(carro));
-                    });
-
-                    //disminuyendo cantidadcon boton -
-                    carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').addEventListener('click', ()=>{
-                        if (producto.cantidad > 1) {
-                            producto.cantidad--;
-                            carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
-                            localStorage.setItem('carroLS',JSON.stringify(carro));
-                        } else {
-                            carroItemDom.classList.add('cart__item--removed');
-                            setTimeout(()=>carroItemDom.remove(),290);
-                            carro = carro.filter(carroItem => (carroItem.nombre !== producto.nombre));
-                            localStorage.setItem('carroLS',JSON.stringify(carro));
-                            botonDomCarro.innerText = "Add to Cart"; //cambiando nombre en boton
-                            botonDomCarro.disabled = false; //reactivando boton                           
-                        };
-                        if (producto.cantidad === 1) {
-                            carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').classList.add('btn--danger');
-                        }
-                    });
-
-                    //Eliminando producto del carro con boton X
-                    carroItemDom.querySelector('[data-action="REMOVE_ITEM"]').addEventListener('click', ()=>{
-                        carroItemDom.classList.add('cart__item--removed');
-                        setTimeout(()=>carroItemDom.remove(),290);
-                        carro = carro.filter(carroItem => (carroItem.nombre !== producto.nombre));
-                        localStorage.setItem('carroLS',JSON.stringify(carro));
-                        botonDomCarro.innerText = "Add to Cart"; //cambiando nombre en boton
-                        botonDomCarro.disabled = false; //reactivando boton
-                    });
-                };
-            });
+            
+            //llamando a funcion de manejo de botones
+            manejoDeBotones(botonDomCarro,producto);
         };
     });
 });
+
+/* =============================================================
+                            Funciones 
+============================================================= */
+function insertandoItemDom(producto) {
+    carroDOM.insertAdjacentHTML('beforeend',`
+        <div class="cart__item">
+            <img class="cart__item__image" src="${producto.imagen}" alt="${producto.nombre}">
+            <h3 class="cart__item__name">${producto.nombre}</h3>
+            <h3 class="cart__item__price">${producto.precio}</h3>
+            <button class="btn btn--primary btn--small ${(producto.cantidad === 1 ? 'btn--danger':'')}"" data-action="DECRESE_ITEM">&minus;</button>
+            <h3 class="cart__item__quantity">${producto.cantidad}</h3>
+            <button class="btn btn--primary btn--small" data-action="INCREASE_ITEM">&plus;</button>
+            <button class="btn btn--danger btn--small" data-action="REMOVE_ITEM">&times;</button>
+        </div>
+    `);
+};
+
+function manejoDeBotones(botonDomCarro,producto) {
+    botonDomCarro.innerText = "In Cart"; //cambiando nombre en boton
+    botonDomCarro.disabled = true; //desactivando boton
+
+    //leyendo botones presionados en el carro y actuando segun el boton activado
+    const carroItemsDOM = carroDOM.querySelectorAll('.cart__item');
+    carroItemsDOM.forEach(carroItemDom => {
+        
+        if (carroItemDom.querySelector('.cart__item__name').innerText === producto.nombre) {
+
+            //incrementado cantidadcon boton +
+            carroItemDom.querySelector('[data-action="INCREASE_ITEM"]').addEventListener('click', ()=>{
+                producto.cantidad++;
+                carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
+                carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').classList.remove('btn--danger');
+                localStorage.setItem('carroLS',JSON.stringify(carro));
+            });
+
+            //disminuyendo cantidadcon boton -
+            carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').addEventListener('click', ()=>{
+                if (producto.cantidad > 1) {
+                    producto.cantidad--;
+                    carroItemDom.querySelector('.cart__item__quantity').innerText = producto.cantidad;
+                    localStorage.setItem('carroLS',JSON.stringify(carro));
+                } else {
+                    botonX(carroItemDom,producto,botonDomCarro); //llamando a funcion del boton x                         
+                };
+                if (producto.cantidad === 1) {
+                    carroItemDom.querySelector('[data-action="DECRESE_ITEM"]').classList.add('btn--danger');
+                };
+            });
+
+            //Eliminando producto del carro con boton X
+            carroItemDom.querySelector('[data-action="REMOVE_ITEM"]').addEventListener('click', ()=>{
+                botonX(carroItemDom,producto,botonDomCarro); //llamando a funcion del boton x 
+            });
+        };
+    });    
+};
+
+function botonX(carroItemDom,producto,botonDomCarro) {
+    carroItemDom.classList.add('cart__item--removed');
+    setTimeout(()=>carroItemDom.remove(),290);
+    carro = carro.filter(carroItem => (carroItem.nombre !== producto.nombre));
+    localStorage.setItem('carroLS',JSON.stringify(carro));
+    botonDomCarro.innerText = "Add to Cart"; //cambiando nombre en boton
+    botonDomCarro.disabled = false; //reactivando boton
+};
 
 
 
